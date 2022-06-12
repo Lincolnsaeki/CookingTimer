9d3b984cc9adbaa88478c7bf6ea84e624dcf6c40
Rinnosuke Saeki <macrin1559@gmail.com>
2022-06-12 13:43:05 +0900

    timercell

--- CookingTimer/TableViewController.swift
+++ CookingTimer/TableViewController.swift
@@ -8,28 +8,38 @@
 import UIKit
 
 class TableViewController: UITableViewController {
-
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
     }
-
+    
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 10
+    
+    //override func numberOfSections(in tableView: UITableView) -> Int {
+    //return 2
+    //}
+    
+    //初期のcellの数
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    //cellのサイズ
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
+        
+        
+        let MInt = UserDefaults.standard.integer(forKey: "minutes")
+        
+        return cell
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
-        
-        cell.Mtimer.text = "00"
-        cell.Stimer.text = "00"
-        cell.TimerNameLabel.text = "Swift"
-            return cell
-        }
+
 
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
