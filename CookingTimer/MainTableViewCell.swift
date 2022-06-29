f78886ef9e3ee773e04b2411d3498125a0113203
Rinnosuke Saeki <macrin1559@gmail.com>
2022-06-30 07:46:10 +0900

    nottimer

--- CookingTimer/MainTableViewCell.swift
+++ CookingTimer/MainTableViewCell.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import RealmSwift
 
 class MainTableViewCell: UITableViewCell {
     
@@ -15,63 +14,4 @@ class MainTableViewCell: UITableViewCell {
     @IBOutlet weak var Starttimer: UIButton!
     @IBOutlet weak var Stoptimer: UIButton!
 
-    //Realmのデータの引き出し方わかんないよ〜そのいち
-    var time = 60
-    
-    var timer = Timer()
-    var delegate: UIViewController? //必要
-    var alert:UIAlertController!
-    let realm = try! Realm()
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        
-        let timerRecords = Array(realm.objects(TimerModel.self))
-        
-               for objData in timerRecords{
-                   timerLabel.text = String(time)
-                   //Realmのデータの引き出し方わかんないよ〜そのに
-                   timerNameLabel.text = "\(TimerModel.timerName)"
-                   
-                   Starttimer.layer.cornerRadius = 10
-               }
-        
-        
-        
-    }
-    
-    // ボタンが押された時の処理
-    @IBAction func startButton(){
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] (timer) in
-            self.time -= 1
-            self.timerLabel.text = String(self.time)
-            
-            if self.time == 0 {
-                timer.invalidate()
-                self.showAlert()
-            }
-        })
-    }
-    @IBAction func stopButton(){
-        timer.invalidate()
-        
-    }
-    
-    func showAlert(){
-        alert = UIAlertController(title:timerNameLabel.text , message: "タイマーが終了しました", preferredStyle: UIAlertController.Style.alert)
-        
-        let OKAction:UIAlertAction = UIAlertAction(title: "OK", style: .default, handler:{
-            (action:UIAlertAction!) -> Void in
-            
-            
-        })
-        
-        //let cancelAction:UIAlertAction = UIAlertAction(title: "Cancel", style: .cancel, handler:{
-            //action in
-        //})
-        
-        //alert.addAction(cancelAction)
-        alert.addAction(OKAction)
-        delegate!.present(alert, animated: true, completion: nil) //必要
-    }
 }
--- CookingTimer/TableViewController.swift
+++ CookingTimer/TableViewController.swift
@@ -12,24 +12,27 @@ class TableViewController: UITableViewController {
     
     let realm = try! Realm()
     var timer = [TimerModel]()
+    var delegate: UIViewController?
+    var alert:UIAlertController!
+
         
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
+        tableView.register(UINib(nibName: "MainTableViewCell", bundle: nil), forCellReuseIdentifier: "MainTableViewCell")
     }
     
     // MARK: - Table view data source
-    
-    func getTimerData() {
-            timer = Array(realm.objects(TimerModel.self)).reversed()
-        // Realm DBから保存されてるツイートを全取得
-            tableView.reloadData() // テーブルビューをリロード
-        }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        timer = Array(realm.objects(TimerModel.self)).reversed()
+    // Realm DBから保存されてるツイートを全取得
+        tableView.reloadData() // テーブルビューをリロード
+    }
 
     //初期のcellの数
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        timer.count
+        return timer.count
     }
     //cellのサイズ
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -37,9 +40,47 @@ class TableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! MainTableViewCell
-        cell.delegate = self
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MainTableViewCell", for: indexPath) as! MainTableViewCell
+        cell.timerLabel.text = String(timer[indexPath.row].seconds)
+        cell.timerNameLabel.text = timer[indexPath.row].timerName
+        cell.Starttimer.addTarget(self, action: #selector(self.startButton(_:)), for: UIControl.Event.touchUpInside)
+        cell.Starttimer.tag = indexPath.row
+        cell.Starttimer.layer.cornerRadius = 10
+        cell.Stoptimer.addTarget(self, action: #selector(self.stopButton(_:)), for: UIControl.Event.touchUpInside)
+
         return cell
     }
+    //ボタンが押されたら
+    @objc func startButton(_ sender: UIButton) {
+        let serected = timer[sender.tag]
+           print(serected)
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { [self] (timer) in
+            self.time -= 1
+            self.timerLabel.text = String(self.time)
+            
+            if self.time == 0 {
+                timer.invalidate()
+                self.showAlert()
+            }
+        })
+        
+        }
+    @objc func stopButton(_ sender: UIButton){
+        timer.invalidate()
+    }
+    
+    func showAlert(){
+        alert = UIAlertController(title: timerNameLabel.text, message: "タイマーが終了しました", preferredStyle: UIAlertController.Style.alert)
+        
+        let OKAction:UIAlertAction = UIAlertAction(title: "OK", style: .default, handler:{
+            (action:UIAlertAction!) -> Void in
+            
+        })
+                
+        alert.addAction(OKAction)
+        delegate!.present(alert, animated: true, completion: nil) //必要
+    }
+    
     
 }
